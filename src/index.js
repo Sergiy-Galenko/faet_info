49f19d532e984bf24a11e83e49e977d6bfc5771e
Serhii-Halenko <galenkosergij830@gmail.com>
2025-08-22 16:18:59 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import Main from "./components/Main";
+import "./styles/global.css";
 
 createRoot(document.getElementById("root")).render(<Main />);
